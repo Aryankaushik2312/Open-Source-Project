36e2466c6dfe25083bc403a45ecf3f9742c0a44e
Aryankaushik2312 <Aryankaushik2312@gmail.com>
2020-06-27 18:25:04 +0530

    News App

---
+++ news_app/lib/secret.dart
@@ -0,0 +1,2 @@
+String apiKey = "bf5add013d604d3ba0715f99d9c39114";
+String country = "in";
---
+++ news_app/lib/views/homepage.dart
@@ -0,0 +1,147 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import '../helper/data.dart';
+import '../helper/widgets.dart';
+import '../models/category_model.dart';
+import 'category_news.dart';
+import '../helper/news.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool _loading;
+  var newslist;
+
+  List<CategoryModel> categories = List<CategoryModel>();
+
+  void getNews() async {
+    News news = News();
+    await news.getNews();
+    newslist = news.news;
+    setState(() {
+      _loading = false;
+    });
+  }
+
+  @override
+  void initState() {
+    _loading = true;
+    super.initState();
+    categories = getCategories();
+    getNews();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: myAppBar(),
+      body: SafeArea(
+        child: _loading
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : SingleChildScrollView(
+                child: Container(
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        padding: EdgeInsets.symmetric(horizontal: 16),
+                        height: 70,
+                        child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: categories.length,
+                          itemBuilder: (context, index) {
+                            return CategoryCard(
+                              imageUrl: categories[index].imageUrl,
+                              categoryName: categories[index].categoryName,
+                            );
+                          },
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(top: 16),
+                        child: ListView.builder(
+                          itemCount: newslist.length,
+                          shrinkWrap: true,
+                          physics: ClampingScrollPhysics(),
+                          itemBuilder: (context, index) {
+                            return NewsTile(
+                              imgUrl: newslist[index].urlToImage ?? "",
+                              title: newslist[index].title ?? "",
+                              desc: newslist[index].description ?? "",
+                              content: newslist[index].content ?? "",
+                              posturl: newslist[index].articleUrl ?? "",
+                            );
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+      ),
+    );
+  }
+}
+
+class CategoryCard extends StatelessWidget {
+  final String imageUrl, categoryName;
+
+  CategoryCard({
+    this.imageUrl,
+    this.categoryName,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => CategoryNews(
+              newsCategory: categoryName.toLowerCase(),
+            ),
+          ),
+        );
+      },
+      child: Container(
+        margin: EdgeInsets.only(right: 14),
+        child: Stack(
+          children: <Widget>[
+            ClipRRect(
+              borderRadius: BorderRadius.circular(5),
+              child: CachedNetworkImage(
+                imageUrl: imageUrl,
+                height: 60,
+                width: 120,
+                fit: BoxFit.cover,
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              height: 60,
+              width: 120,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5),
+                color: Colors.black26,
+              ),
+              child: Text(
+                categoryName,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
